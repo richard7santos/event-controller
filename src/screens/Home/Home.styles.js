e308cf1ee73d7329fd76828e3cfe82b417d3384c
r1llary <ana.rillaryy@gmail.com>
2025-04-07 21:14:42 -0300

    Event Ana Rillary

--- src/screens/Home/Home.styles.js
+++ src/screens/Home/Home.styles.js
@@ -3,15 +3,32 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#fff',
+        paddingTop: 85,
+    },
+    container2:{
+        flex: 1,
+        alignItems: 'start',
+        backgroundColor: '#fff',
+        paddingTop: 70,
     },
     text: {
-        fontSize: 28,
+        fontSize: 40,
         fontWeight: 'bold',
-        color: '#000',
+        color: '#FE2E2E',
         marginBottom: 12,
+    },
+    demais:{
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#333',
+        marginBottom: 12,
+    },
+    respostas:{
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: 'red',
     },
     count: {
         fontSize: 18,
@@ -32,5 +49,12 @@ export const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.3,
         shadowRadius: 4,
+    },
+
+    alert: {
+        fontSize: 20,
+        color: 'red',
+        fontWeight: 'bold',
+        marginTop: 10,
     },
 });
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
@@ -8,6 +8,11 @@ import { styles } from './Home.styles';
 const Home = () => {
     const navigation = useNavigation();
     const { participantes } = useContext(AppContext);
+    const [lotacao, setLotacao] = useState(2);
+    const vagas = lotacao - participantes.length;
+    const isLotado = participantes.length >= lotacao;
+   
+
 
     const goToAddParticipante = () => {
         navigation.navigate('AddParticipante');
@@ -15,16 +20,27 @@ const Home = () => {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Home</Text>
-            <Text style={styles.count}>
-                Participantes cadastrados: {participantes.length}
-            </Text>
-
-            <TouchableOpacity style={styles.btn} onPress={goToAddParticipante}>
+            <Text style={styles.text}>AI TECHNOLOGY EVENT </Text>
+            
+            <View style={styles.container2}>
+            <Text style={styles.demais}>DATA:  <Text style={styles.respostas}>08/03/2026</Text></Text>
+            <Text style={styles.demais}>LOCAL: <Text style={styles.respostas}>CASTELLI HALL</Text></Text>
+            <Text style={styles.demais}>LOTAÇÃO: <Text style={styles.respostas}>{lotacao}</Text></Text>
+            <Text style={styles.demais}>VAGAS: <Text style={styles.respostas}>{vagas}</Text></Text>
+            <Text style={styles.demais}>PARTICIPANTES CADASTRADOS:  <Text style={styles.respostas}>{participantes.length}
+            </Text></Text>
+            <Text style={styles.demais}> VAGAS DISPONÍVEIS: <Text style={styles.respostas}>{vagas} 
+            </Text></Text>
+            
+            {isLotado && (
+                <Text style={styles.alert}> 🚫LIMITE DE PARTICIPANTES ATINGIDO🚫</Text>
+            )}
+            </View>
+            <TouchableOpacity disabled={isLotado} style ={styles.btn} onPress={goToAddParticipante}>
                 <Ionicons name="add" size={32} color="#fff" />
             </TouchableOpacity>
         </View>
     );
-};
+}; 
 
 export default Home;
