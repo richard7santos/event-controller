fc5fd0b93be1f531a3bd4560c68bc0be2c44f1f3
richard7santos <richard7santos@hotmail.com>
2025-09-15 13:02:04 -0300

    icons

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,46 +1,45 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import UserProfile from '../screens/UserProfile';
 import { EventList } from '../screens/EventListScreen';
 import UserEventsList from '../screens/UserEventList';
-
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
     return (
-        <Tab.Navigator initialRouteName="Eventos">
-            <Tab.Screen
-                name="Eventos"
-                component={EventList}
-                options={{
-                    headerStyle: {
-                        backgroundColor: '#0F62AC',
-                    },
-                    headerTintColor: '#fff',
-                }}
-            />
-            <Tab.Screen
-                name="Meus Eventos"
-                component={UserEventsList}
-                options={{
-                    headerStyle: {
-                        backgroundColor: '#0F62AC',
-                    },
-                    headerTintColor: '#fff',
-                }}
-            />
-            <Tab.Screen
-                name="Perfil"
-                component={UserProfile}
-                options={{
-                    headerStyle: {
-                        backgroundColor: '#0F62AC',
-                    },
-                    headerTintColor: '#fff',
-                }}
-            />
+        <Tab.Navigator
+            initialRouteName="Eventos"
+            screenOptions={({ route }) => ({
+                headerStyle: {
+                    backgroundColor: '#0F62AC',
+                },
+                headerTintColor: '#fff',
+                tabBarActiveTintColor: '#0F62AC',
+                tabBarInactiveTintColor: 'gray',
+                tabBarStyle: {
+                    backgroundColor: '#fff',
+                },
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
 
+                    if (route.name === 'Eventos') {
+                        iconName = focused ? 'calendar' : 'calendar-outline';
+                    } else if (route.name === 'Meus Eventos') {
+                        iconName = focused ? 'list' : 'list-outline';
+                    } else if (route.name === 'Perfil') {
+                        iconName = focused ? 'person' : 'person-outline';
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+        >
+            <Tab.Screen name="Eventos" component={EventList} />
+            <Tab.Screen name="Meus Eventos" component={UserEventsList} />
+            <Tab.Screen name="Perfil" component={UserProfile} />
         </Tab.Navigator>
     );
 };
